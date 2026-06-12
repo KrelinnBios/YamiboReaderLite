d8e3133fc7020402b24e76c0d32ba590c3afbf41
Bios <krelinn@outlook.com>
2026-06-13 00:49:09 +0800

    更新 ThreadHtmlParserTest.kt

--- app/src/test/java/org/shirakawatyu/yamibo/novel/parser/ThreadHtmlParserTest.kt
+++ app/src/test/java/org/shirakawatyu/yamibo/novel/parser/ThreadHtmlParserTest.kt
@@ -44,6 +44,67 @@ class ThreadHtmlParserTest {
     }
 
     @Test
+    fun parseReaderPage_filtersAuthorChatterAndQuotesFromStructuredAnthology() {
+        val chapterBody = "正文段落。".repeat(80)
+        val html = """
+            <html>
+            <body>
+              <h1 id="thread_subject">短篇合集</h1>
+              <div id="post_1">
+                <a id="postnum1"><em>1</em></a>
+                <a href="forum.php?mod=viewthread&tid=1&authorid=100">只看该作者</a>
+                <table><tbody><tr><td class="t_f" id="postmessage_1">
+                  合集说明，会包含数个短篇故事和权限内容。
+                </td></tr></tbody></table>
+              </div>
+              <div id="post_2">
+                <a href="forum.php?mod=viewthread&tid=1&authorid=100">只看该作者</a>
+                <table><tbody><tr><td class="t_f" id="postmessage_2">
+                  <i class="pstatus">本帖最后由 作者 编辑</i>
+                  第一章 造化弄人<br>$chapterBody
+                </td></tr></tbody></table>
+              </div>
+              <div id="post_3">
+                <a href="forum.php?mod=viewthread&tid=1&authorid=100">只看该作者</a>
+                <table><tbody><tr><td class="t_f" id="postmessage_3">
+                  今天写了很久怎么还没写完，这是作者闲聊。
+                </td></tr></tbody></table>
+              </div>
+              <div id="post_4">
+                <a href="forum.php?mod=viewthread&tid=1&authorid=100">只看该作者</a>
+                <table><tbody><tr><td class="t_f" id="postmessage_4">
+                  <div class="quote"><blockquote>读者问：这是测试权限吗？</blockquote></div>
+                  是的，只是测试。
+                </td></tr></tbody></table>
+              </div>
+              <div id="post_5">
+                <a href="forum.php?mod=viewthread&tid=1&authorid=100">只看该作者</a>
+                <table><tbody><tr><td class="t_f" id="postmessage_5">
+                  第三章 新故事<br>
+                  <div class="locked-content"><span class="locked-tip">30</span>$chapterBody</div>
+                </td></tr></tbody></table>
+              </div>
+            </body>
+            </html>
+        """.trimIndent()
+
+        val result = ThreadHtmlParser.parseReaderPage(
+            html = html,
+            baseUrl = "https://bbs.yamibo.com/forum.php?mod=viewthread&tid=1",
+            expectedAuthorId = "100"
+        )
+
+        assertTrue(result.combinedHtml.contains("合集说明"))
+        assertTrue(result.combinedHtml.contains("第一章 造化弄人"))
+        assertTrue(result.combinedHtml.contains("第三章 新故事"))
+        assertFalse(result.combinedHtml.contains("作者闲聊"))
+        assertFalse(result.combinedHtml.contains("读者问"))
+        assertFalse(result.combinedHtml.contains("只是测试"))
+        assertFalse(result.combinedHtml.contains("本帖最后由"))
+        assertFalse(result.combinedHtml.contains("locked-tip"))
+    }
+
+    @Test
     fun parseMangaPage_usesOriginalAttachmentUrl() {
         val html = """
             <html>
