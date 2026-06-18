a07434564afce5863ac5710cf00fa57a20e05d88
Bios <162010446+KrelinnBios@users.noreply.github.com>
2026-06-18 17:26:58 +0800

    修复 Dependabot 构建依赖告警

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,30 @@
+buildscript {
+    configurations.classpath {
+        resolutionStrategy.eachDependency {
+            when ("${requested.group}:${requested.name}") {
+                "org.apache.commons:commons-compress" -> useVersion("1.28.0")
+                "org.bouncycastle:bcprov-jdk18on",
+                "org.bouncycastle:bcpkix-jdk18on",
+                "org.bouncycastle:bcutil-jdk18on" -> useVersion("1.84")
+                "org.bitbucket.b_c:jose4j" -> useVersion("0.9.6")
+                "io.netty:netty-buffer",
+                "io.netty:netty-codec",
+                "io.netty:netty-codec-http",
+                "io.netty:netty-codec-http2",
+                "io.netty:netty-codec-socks",
+                "io.netty:netty-common",
+                "io.netty:netty-handler",
+                "io.netty:netty-handler-proxy",
+                "io.netty:netty-resolver",
+                "io.netty:netty-transport",
+                "io.netty:netty-transport-native-unix-common" -> useVersion("4.1.135.Final")
+            }
+        }
+    }
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
